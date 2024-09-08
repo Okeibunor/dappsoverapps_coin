e28bb0f018e2cef3f7024b9f435653f67bef3778
Favour Okeibunor <favourokeibunor@gmail.com>
2024-09-08 14:50:25 +0100

    modify project to dappsoverapps coin share

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                primaryBg: '#000',
+                assetBg: '#c71585',
+                secondaryBg: '#551a8b',
+                primaryText: '#fff'
+            }
+        }
+    }
+};
